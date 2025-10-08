e204d5dbe2e51a2979a984f663768ee9ef0f9b4e
cristianCR0 <cristiancorella@estudiantec.cr>
2025-10-08 01:04:19 -0600

    Jump Bug

--- Assets/Asset Store/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/Asset Store/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -216,6 +216,7 @@ namespace StarterAssets
 				{
 					// the square root of H * -2 * G = how much velocity needed to reach desired height
 					_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+					Debug.Log($"SALTANDO! Fuerza: {_verticalVelocity}");
 				}
 
 				// jump timeout
@@ -244,6 +245,7 @@ namespace StarterAssets
 			{
 				_verticalVelocity += Gravity * Time.deltaTime;
 			}
+			Debug.Log($"Grounded: {Grounded} | VerticalVelocity: {_verticalVelocity} | Gravity: {Gravity}");
 		}
 
 		private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
